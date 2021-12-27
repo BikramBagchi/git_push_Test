11e42cff3f21d9937ad27f8fd928229f219e8f21
unknown <bikrambagchi1994@gmail.com>
2021-12-28 00:08:19 +0530

    test two

--- src/git_uploaded_via_command/Test_one.java
+++ src/git_uploaded_via_command/Test_one.java
@@ -1,12 +1,15 @@
 package git_uploaded_via_command;
 
-public class Test_one {
+public class Test_one 
+{
 
 	public static void main(String[] args)
 	{
 	
 		
 		System.out.println("This is for test purpose");
+		
+		System.out.println("This is also for test purpose");
 
 	}
 
---
+++ src/git_uploaded_via_command/Test_two.java
@@ -0,0 +1,13 @@
+package git_uploaded_via_command;
+
+public class Test_two {
+
+	public static void main(String[] args)
+	
+	{
+		System.out.println("Test test");
+
+		
+	}
+
+}
